690616374e630bfabce448d983a0532ddc018fe2
Middlechild0 <jimmymathu28@gmail.com>
2025-02-19 12:40:18 +0300

    image to legacy

--- app/components/pictures.tsx
+++ app/components/pictures.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 export const pictures = {
   house1: <Image src="/images/house-1.png" alt="House 1" width={100} height={100} quality={100} layout="responsive" />,
