b185df5878e4e3575fe205c3534058ad153da01d
Middlechild0 <jimmymathu28@gmail.com>
2025-02-19 12:41:08 +0300

    image

--- app/components/pictures.tsx
+++ app/components/pictures.tsx
@@ -1,12 +1,82 @@
-import Image from "next/legacy/image";
+import Image from "next/image";
 
 export const pictures = {
-  house1: <Image src="/images/house-1.png" alt="House 1" width={100} height={100} quality={100} layout="responsive" />,
-  house2: <Image src="/images/house-2.png" alt="House 2" width={100} height={100} quality={100} layout="responsive" />,
-  house3: <Image src="/images/house-3.png" alt="House 3" width={100} height={100} quality={100} layout="responsive" />,
-  house4: <Image src="/images/house-4.png" alt="House 4" width={100} height={100} quality={100} layout="responsive" />,
-  house5: <Image src="/images/house-5.jpeg" alt="House 5" width={100} height={100} quality={100} layout="responsive" />,
-  house6: <Image src="/images/house-6.jpeg" alt="House 6" width={100} height={100} quality={100} layout="responsive" />,
-  house7: <Image src="/images/house-7.jpeg" alt="House 7" width={100} height={100} quality={100} layout="responsive" />,
+  house1: <Image
+    src="/images/house-1.png"
+    alt="House 1"
+    width={100}
+    height={100}
+    quality={100}
+    sizes="100vw"
+    style={{
+      width: "100%",
+      height: "auto"
+    }} />,
+  house2: <Image
+    src="/images/house-2.png"
+    alt="House 2"
+    width={100}
+    height={100}
+    quality={100}
+    sizes="100vw"
+    style={{
+      width: "100%",
+      height: "auto"
+    }} />,
+  house3: <Image
+    src="/images/house-3.png"
+    alt="House 3"
+    width={100}
+    height={100}
+    quality={100}
+    sizes="100vw"
+    style={{
+      width: "100%",
+      height: "auto"
+    }} />,
+  house4: <Image
+    src="/images/house-4.png"
+    alt="House 4"
+    width={100}
+    height={100}
+    quality={100}
+    sizes="100vw"
+    style={{
+      width: "100%",
+      height: "auto"
+    }} />,
+  house5: <Image
+    src="/images/house-5.jpeg"
+    alt="House 5"
+    width={100}
+    height={100}
+    quality={100}
+    sizes="100vw"
+    style={{
+      width: "100%",
+      height: "auto"
+    }} />,
+  house6: <Image
+    src="/images/house-6.jpeg"
+    alt="House 6"
+    width={100}
+    height={100}
+    quality={100}
+    sizes="100vw"
+    style={{
+      width: "100%",
+      height: "auto"
+    }} />,
+  house7: <Image
+    src="/images/house-7.jpeg"
+    alt="House 7"
+    width={100}
+    height={100}
+    quality={100}
+    sizes="100vw"
+    style={{
+      width: "100%",
+      height: "auto"
+    }} />,
   // Add more images here as needed
 };
